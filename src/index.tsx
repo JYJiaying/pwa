e8a661d3286ed87f5128edf0bb3a9b3d7209e029
He, Jiaying <he.jiaying@student.hs-anhalt.de>
2023-06-09 01:15:47 +0200

    link hinzufügen

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Routes, Route } from 'react-router-dom'
 
+import Anmelden from './components/Anmelden';
+import Registrieren from './components/Registrieren';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import './App.css'
@@ -10,9 +12,11 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-  <Router>
-  <App/>
-</Router>
+  <Routes>
+        <Route path="/pwa/" element={<App/>}/>
+        <Route path="/pwa/anmelden/" element={<Anmelden/>} />
+        <Route path="/pwa/registrieren/" element={<Registrieren/>} />
+      </Routes>
 
-  // </React.StrictMode>,
+  // </React.StrictMode>
 )
