023ce6edc57d03ca53b6b4d0b1989a36c6e70f10
He, Jiaying <he.jiaying@student.hs-anhalt.de>
2023-06-08 23:07:55 +0200

    link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // npm i react-router-dom
 import { Routes, Route } from 'react-router-dom'
-import {useState } from 'react';
+import { useState } from 'react';
 import Navbar from "./components/Navbar";
 import Toggler from "./components/Toggler";
 import Anmelden from './components/Anmelden';
@@ -10,19 +10,22 @@ function App() {
   const [togglerVisible, setTogglerVisibility] = useState(false);
 
   return (
-    <Routes>
-      <Route path="https://JYJiaying.github.io/pwa" element={<div className="flex-column p-3 ">
-        <Navbar click={() => setTogglerVisibility(!togglerVisible)} />
-        {togglerVisible && <Toggler click={() => setTogglerVisibility(false)} />}
-        <Videosbereich/>
-       </div>
-      } />
-      <Route path="https://JYJiaying.github.io/pwa/Anmelden" element={<Anmelden/>}/>
-      <Route path="./Registrieren/" element={<Registrieren/>}/>
+    <div>
+      <Routes>
+        <Route path="https://JYJiaying.github.io/pwa/" element={<div className="flex-column p-3 ">
+          <Navbar click={() => setTogglerVisibility(!togglerVisible)} />
+          {togglerVisible && <Toggler click={() => setTogglerVisibility(false)} />}
+          <Videosbereich />
+        </div>
+        } />
 
-
-
-    </Routes>
+      </Routes>
+      <Routes>
+        <Route path="https://JYJiaying.github.io/pwa/Anmelden" element={<Anmelden />} />
+      </Routes><Routes>
+        <Route path="https://JYJiaying.github.io/pwa//Registrieren" element={<Registrieren />} />
+      </Routes>
+    </div>
   );
 }
 export default App;  
