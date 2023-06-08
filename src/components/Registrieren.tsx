536cf0cc5b8bf1b6a8759c0a28518e9145012096
He, Jiaying <he.jiaying@student.hs-anhalt.de>
2023-06-09 00:39:52 +0200

    link

--- src/components/Registrieren.tsx
+++ src/components/Registrieren.tsx
@@ -11,7 +11,7 @@ function Registrieren() {
                 <Navbar click={() => setTogglerVisibility(!togglerVisible)} />
                 {togglerVisible && <Toggler click={() => setTogglerVisibility(false)} />}
                 <div className="btn-group p-5 mt-4 d-flex justify-content-center">
-                    <a href="/pwa/Anmelden" className="btn btn-primary " aria-current="page">Anmelden</a>
+                    <a href="/pwa/Anmelden/" className="btn btn-primary " aria-current="page">Anmelden</a>
                     <a href="/pwa/Registrieren/" className="btn btn-primary active">Jetzt Registrieren</a>
                 </div>
                 <div className="mb-4">
