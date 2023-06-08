360e40a5827a3c7d2e152a9a22d353490f9f7c3f
He, Jiaying <he.jiaying@student.hs-anhalt.de>
2023-06-08 23:14:48 +0200

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,11 @@ function App() {
         <Route path="https://JYJiaying.github.io/pwa/" element={<div className="flex-column p-3 ">
           <Navbar click={() => setTogglerVisibility(!togglerVisible)} />
           {togglerVisible && <Toggler click={() => setTogglerVisibility(false)} />}
-          <Videosbereich />
-        </div>
-        } />
+          <Videosbereich /> </div>} >
 
-      </Routes>
-      <Routes>
         <Route path="https://JYJiaying.github.io/pwa/Anmelden" element={<Anmelden />} />
-      </Routes><Routes>
         <Route path="https://JYJiaying.github.io/pwa//Registrieren" element={<Registrieren />} />
+        </Route>
       </Routes>
     </div>
   );
