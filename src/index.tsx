462046b27552ade2e396f5851edefdfc25980709
He, Jiaying <he.jiaying@student.hs-anhalt.de>
2023-06-09 00:55:34 +0200

    createHashRouter löschen

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Routes, Route, HashRouter } from 'react-router-dom'
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
 
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -12,16 +8,11 @@ import './index.css'
 import './App.css'
 
 import { BrowserRouter as Router } from 'react-router-dom'
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
   <HashRouter>
- <RouterProvider router={router} />
+  <App/>
 </HashRouter>
 
   // </React.StrictMode>,
