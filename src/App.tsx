43d515b8a979c2999e350807e4c9faab68ac7840
He, Jiaying <he.jiaying@student.hs-anhalt.de>
2023-06-09 10:41:24 +0200

    Hashroute

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ function App() {
   const [togglerVisible, setTogglerVisibility] = useState(false);
 
   return (
-      <Routes>
+      <HashRouter>
         <Route path="/pwa/" element={<div className="flex-column p-3 ">
           <Navbar click={() => setTogglerVisibility(!togglerVisible)} />
           {togglerVisible && <Toggler click={() => setTogglerVisibility(false)} />}
           <Videosbereich /> </div>}/>
         <Route path="/pwa/anmelden/" element={<Anmelden/>} />
         <Route path="/pwa/registrieren/" element={<Registrieren/>} />
-      </Routes>
+      </HashRouter>
       
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
@@ -10,9 +10,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-  <Router>
+  <HashRouter>
   <App/>
-</Router>
+</HashRouter>
 
   // </React.StrictMode>
 )
